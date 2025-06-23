07e6862fda2cbb06a71cbed82e753aad5bafcb9c
DevHamaad <hamaadmajeed786@gmail.com>
2025-06-23 12:41:59 +0500

    added all the listviews in the drawer

--- lib/main.dart
+++ lib/main.dart
@@ -126,7 +126,85 @@ class MyApp extends StatelessWidget {
                   color: Colors.black.withAlpha(100),
                   height: 10,
                 ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10,),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.cyclone),
+                        title: Text("Connections",style: TextStyle(fontWeight: FontWeight.w600),),
+                        trailing: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 4),
+                          decoration: BoxDecoration(
+                              color: Colors.pink.withAlpha(20),
+                              borderRadius: BorderRadius.circular(5),
+                              border:Border.all(
+                                color: Colors.black.withAlpha(100),
+                              )
+                          ),
+                          child: const Text(
+                            'New',
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.black.withAlpha(100),
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10,),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.person_4),
+                        title: Text("Account",style: TextStyle(fontWeight: FontWeight.w600),),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.edit_rounded),
+                        title: Text("Edit Profile",style: TextStyle(fontWeight: FontWeight.w600),),
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.black.withAlpha(100),
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10,),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.dark_mode_rounded),
+                        title: Text("Dark Mode",style: TextStyle(fontWeight: FontWeight.w600),),
+                        trailing: Switch(value: false, onChanged: (value){}),
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.black.withAlpha(100),
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10,),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.logout_rounded),
+                        title: Text("Logout",style: TextStyle(fontWeight: FontWeight.w600),),
+                      ),
 
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
