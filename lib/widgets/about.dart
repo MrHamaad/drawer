b53b0a52f6da65c5e6bfc9d235fdb688c1b1a6f7
DevHamaad <hamaadmajeed786@gmail.com>
2025-06-23 12:57:59 +0500

    create home,about,setting widgets

---
+++ lib/widgets/about.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AboutWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("This is the About Screen", style: TextStyle(fontSize: 24)));
+  }
+}
---
+++ lib/widgets/setting.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SettingsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("This is the Settings Screen", style: TextStyle(fontSize: 24)));
+  }
+}
